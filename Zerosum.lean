4a51f40f4f943b1b401bee3fffdb756434e66e48
Ma Jia-Jun <hoxide@gmail.com>
2023-10-22 00:25:42 +0800

    up

--- Zerosum.lean
+++ Zerosum.lean
@@ -69,6 +69,9 @@ instance coe_fun : CoeFun (S α) fun _ => α → NNReal :=
 noncomputable def pure (i : α) : S α  := ⟨ fun j => if i=j then 1 else 0,
  by {simp only [Finset.sum_ite_eq, Finset.mem_univ, ite_true]}⟩ 
 
+noncomputable def wsum {α : Type*} [Fintype α] (x : S α) (f : α → ℝ ) := Finset.sum Finset.univ (fun i:α => (x i) * (f i))
+
+lemma sum_pos {α : Type*} [Fintype α] {x : S α} {f : α → ℝ } (H : ∀ i, f i >0) : wsum x f > 0:= by sorry    
 
 end S
 
@@ -133,8 +136,7 @@ lemma simplex_ge_iff_vertex_ge [Fintype I] {f : I → ℝ } {v : ℝ} :
 
 
 -- expactation of the payoff of a mixed stratage
-noncomputable def E (x : S I) (y : S J) : ℝ := Finset.sum Finset.univ  
-( fun (i : I ) => Finset.sum Finset.univ  (fun (j : J) => (x i) * (A i j) * (y j)))
+noncomputable def E (A : I →J →ℝ) (x : S I) (y : S J) : ℝ := wsum x (fun i => wsum y (A i)) 
 
 -- One may need Finset.sum_comm' Finset.sum_mul
 
@@ -151,13 +153,104 @@ lemma E_eq1 {x : PMF I} {y : PMF J} : Finset.sum (@Finset.univ _ A.FI)
 -- invoke exterem value theorem is_compact.exists_forall_le
 
 
-theorem Loomis (B : I →J → ℝ   ) (PB : ∀ i:I, ∀ j:J,  B i j > 0 )  : 
-  ∃ (xx : S I) (yy : S J) (v : ℝ),  
-    (∀ j , sumxC j xx A ≥  v * sumxC j xx B) ∧
-    (∀ i ,  sumyC i yy A ≤  v * sumyC i yy B) := by sorry  
+-- def lam0 
+
+end zerosumFGame
+
+section Loomis
+variable (n : ℕ) {I J: Type*} [Inhabited I] [Inhabited J] [Fintype I] [Fintype J] 
+
+lemma nonempty (α : Type*) [Inhabited α] [Fintype α ]: Finset.Nonempty (@Finset.univ α  _)  := by {
+  use Inhabited.default
+  simp only [Finset.mem_univ]
+}
+
+
+noncomputable def lam.aux (A B : I →J → ℝ )(x : S I) (j : J) :=  wsum x (fun i => A i j ) / wsum x (fun i => B i j) 
+noncomputable def lam.aux' (A B : I →J → ℝ ) (x : S I) :=  Finset.sup' Finset.univ (nonempty J) (lam.aux A B x)  
+
+noncomputable def lam0 (A B : I →J → ℝ ):=  iInf (lam.aux' A B)  
+
+
+noncomputable def mu.aux (A B : I →J → ℝ ) (y : S J) (i : I) :=  wsum y (fun j => A i j ) / wsum y (fun j => B i j) 
+noncomputable def mu.aux' (A B : I →J → ℝ ) (y : S J) :=  Finset.inf' Finset.univ (nonempty  I) (mu.aux A B y)  
+
+noncomputable def mu0 (A B : I →J → ℝ ):=  iSup (mu.aux' A B)  
+
+
+theorem Loomis' (Hgt : 2 ≤ n) (Hn: n=Fintype.card I + Fintype.card J) (A : I →J→ ℝ) (B : I→ J→ ℝ) (PB : ∀ i:I, ∀ j:J,  B i j > 0 ): 
+  ∃ (v : ℝ),  
+    (∃  (xx : S I) , ∀ j , wsum xx (fun i => A i j) ≥  v * wsum xx (fun i=> B i j)) ∧
+    (∃ (yy : S J), ∀ i ,  wsum yy (A i) ≤  v * wsum yy (B i)) := by {
+      induction n, Hgt using Nat.le_induction
+      . {
+        have HSI : Fintype.card I =1 := by {
+          have p1 := @Fintype.card_pos I _ _
+          have p2 := @Fintype.card_pos J _ _
+          linarith
+        }
+        have HSJ : Fintype.card J =1 := by {
+          have p1 := @Fintype.card_pos I _ _
+          have p2 := @Fintype.card_pos J _ _
+          linarith
+        }
+        obtain ⟨i0, hi0⟩ := Fintype.card_eq_one_iff.1 HSI  
+        obtain ⟨j0, hj0⟩ := Fintype.card_eq_one_iff.1 HSJ  
+        have UI : @Finset.univ I _ = {i0} := by {
+          ext
+          simp only [Finset.mem_univ, Finset.mem_singleton, true_iff,hi0]
+        } 
+        have UJ : @Finset.univ J _ = {j0} := by {
+          ext
+          simp only [Finset.mem_univ, Finset.mem_singleton, true_iff,hj0]
+        } 
+        -- v = A i j /B i j where i j is the default value
+        let v := A i0 j0/ B i0 j0 
+        use v
+        constructor 
+        . {
+          use pure I i0 
+          intro j
+          have HJ := hj0 j
+          simp_rw [wsum, HJ, UI,Finset.sum_singleton, _root_.pure, ite_true, NNReal.coe_one, one_mul, ge_iff_le]
+          have :=PB i0 j0
+          calc 
+           A i0 j0 / B i0 j0 * B i0 j0 = A i0 j0 := by {
+            exact div_mul_cancel (A i0 j0) (ne_of_gt this) 
+           } 
+           _ ≤ A i0 j0 := le_refl _ 
+        }
+        . {
+            sorry
+        }
+      }
+      . {sorry}
+    } 
+
+
+theorem Loomis (A : I →J→ ℝ) (B : I→ J→ ℝ) (PB : ∀ i:I, ∀ j:J,  B i j > 0 ): 
+  ∃ (v : ℝ) ,
+  (∃  (xx : S I) , ∀ j , wsum xx (fun i => A i j) ≥  v * wsum xx (fun i=> B i j)) 
+  ∧ (∃ (yy : S J), ∀ i ,  wsum yy (A i) ≤  v * wsum yy (B i)) 
+ := by { 
+      let n := Fintype.card I + Fintype.card J
+      exact @Loomis' n I J _ _ ( by 
+         {
+          have p1 := @Fintype.card_pos I _ _
+          have p2 := @Fintype.card_pos J _ _
+          linarith
+        }
+      ) (rfl) A B PB  
+  } 
+
+
+end Loomis 
 
 
 
+
+
+section minmax
 theorem minmax_theorem : ∃ (xx : S I) (yy : S J) (v : ℝ), (∀ (y : S J), E A xx y ≥ v ) ∧ (∀ (x : S I), E A x yy ≤ v)  := by {  
   let B : I → J → ℝ  := fun i => fun j => 1 
   obtain ⟨xx, yy, v, H1, H2⟩ := Loomis A B (by {intro i j; simp only [gt_iff_lt, zero_lt_one]})
@@ -176,7 +269,4 @@ theorem minmax_theorem : ∃ (xx : S I) (yy : S J) (v : ℝ), (∀ (y : S J), E 
 } 
 
 
-
-end zerosumFGame
-
-
+end minmax
