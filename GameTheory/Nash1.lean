718135f4d925e362e48ba4058474d5d8e1b8e4e0
Ma, Jia-Jun <hoxide@gmail.com>
2024-09-28 17:42:33 +0800

    up

--- GameTheory/Nash1.lean
+++ GameTheory/Nash1.lean
@@ -34,80 +34,6 @@ end Game
 
 open Game
 
-/-
-namespace aux
-universe u v
-
-variable {I : Type u}
-variable {f : I → Type v}
-
-
-/-
-@[simp]
-noncomputable abbrev lifti {i j:I}  (h : j=i) (x : f i): f j :=  by rw [h]; exact x
-
-variable {i j:I} (h : j=i)
-
-noncomputable example : lifti f (Eq.symm h) (lifti f  h (x : f i)) = x  :=  by simp
-
--/
-@[simp]
-abbrev I' (i : I) := {j // j ≠ i}
-
-@[simp]
-abbrev IFun (f : I → Sort*) :=  (i : I) → f i
-
-@[simp]
-abbrev IFun' (f : I → Sort*) (i : I) := (j : I' i) → f j
-
-@[simp]
-noncomputable def with_hole (x : IFun f) (i : I) (y : f i) : IFun f :=
-fun j =>  if h : j = i then (by rw [h];exact y) else x j
-
-
-@[simp]
-noncomputable def combineSubtypeFun (x : f i) (y : IFun' f i ) : IFun f := fun j =>  (if h : j=i then (by rw [h];exact x) else  y ⟨j, h⟩)
-
-@[simp]
-noncomputable def combinePair(b : (f i) × (IFun' f i)) : IFun f := combineSubtypeFun b.1 b.2
-
-
-
-attribute [coe] combinePair
-
-noncomputable instance combinePair.general {i : I}: CoeOut ((f i) × (IFun' f i)) (IFun f) where
-  coe := combinePair
-
-@[simp]
-lemma comma_eval  {x : f i} {y : IFun' f i } : ((x,y): IFun f) i = x := by simp
-
-@[simp]
-lemma comma_eval' {i j:I} {x : f i} {y : IFun' f i } :
-(h : j≠i) →  ((x,y): IFun f) j = y ⟨j,h⟩  := by intro h; simp [h]
-
-
-
-variable (i : I) (b' : I' i → ℝ ) (x : ℝ)
-
-variable {α : Type*}
-#check IFun' (fun _ => α) i
-#check IFun (fun _ => α)
-
-noncomputable instance combinePair.mono  {i : I} : CoeOut ((α) × (I' i→ α )) (I → α ) where
-  coe := @combinePair I (fun _=> α) i
-
-#check ((x,b'): I→ ℝ)
-
-
-noncomputable instance {G:Game} {i : G.I} : CoeOut  ((G.SS i)×(@IFun' G.I G.SS i)) (IFun G.SS) where
-  coe := @combinePair G.I G.SS i
-
-
-end aux
-
-
--/
-
 structure FinGame extends Game where
   FinI : Fintype I
   FinSS : ∀ i : I , Fintype (SS i)
@@ -168,6 +94,7 @@ lemma mixed_g_eq_evaluate (i : G.I) (σ : G.mixedS) : evaluate_at_mixed G i σ =
 
   sorry-/
 
+
 noncomputable def mixed_strategy_of_pure (i : G.I) (a : G.SS i) : S (G.SS i) := by
   let f := fun t ↦ if t = a then (1 : ℝ) else 0
   use f
@@ -176,7 +103,7 @@ noncomputable def mixed_strategy_of_pure (i : G.I) (a : G.SS i) : S (G.SS i) := 
   simp
 
 noncomputable def g_function (i : G.I) (σ : G.mixedS) (a : G.SS i) : ℝ :=
-  σ i a + max 0 (mixed_g i (with_hole σ i (mixed_strategy_of_pure G i a)) - mixed_g i σ)
+  σ i a + max 0 (mixed_g i (Function.update σ i (mixed_strategy_of_pure G i a)) - mixed_g i σ)
 
 variable (sigma : G.mixedS ) (i : G.I) (a : G.SS i)
 --#check evaluate_at_mixed G i sigma
