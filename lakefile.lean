ec9a1f2ad5d8a84b8fa95ab71939a39216a621c2
Lazyfill <2773557821@qq.com>
2025-08-24 17:40:02 +0800

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,22 +1,17 @@
 import Lake
 open Lake DSL
 
-package «GameTheory» {
+package «Gametheory» {
   -- add package configuration options here
 }
 
-lean_lib «GameTheory» {
-
+@[default_target]
+lean_lib «Gametheory» {
   -- add library configuration options here
 }
 
-@[default_target]
-lean_exe «Game» {
-  root := `Main
-}
-
-require llmlean from git
-  "https://github.com/jiajunma/llmlean.git"@"main"
+--require llmlean from git
+--  "https://github.com/jiajunma/llmlean.git"@"main"-/
 
 --require LeanCodePrompts from git "https://github.com/siddhartha-gadgil/LeanAide"@"main"
 
